763b5632e75ead8d5795edb28c71e63aba797a01
Wera Barinova <alisawera@yandex.ru>
2024-04-15 21:20:36 +0300

    Добавлена возможность печатать ошибки в консоль

--- mathtest/src/main/java/mathtest/DBsrc.java
+++ mathtest/src/main/java/mathtest/DBsrc.java
@@ -7,6 +7,8 @@ import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;    
 import org.hibernate.SessionFactory;    
 import org.hibernate.Transaction;  
@@ -16,15 +18,19 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class DBsrc {
+	private static final Logger consolelog = LogManager.getLogger(DBsrc.class);
 	private Properties prop = new Properties();
 	private StandardServiceRegistry ssr;
 	private Metadata meta;
     
 	public DBsrc() {
 		String pss;
+		consolelog.error("\n\n\n\nWriting to console.");
 		try {
 			pss = readPgpass("academy_top", "tutor");
+			consolelog.info("Data Base connected.");
 		}  catch (IOException ioe) {
+			consolelog.info("Password reading error.");
 			pss = "";
 		}
 		prop.setProperty("hibernate.connection.password", pss);
@@ -43,7 +49,6 @@ public class DBsrc {
     	String pss = "";
     	String line = "";
     	String[] parts;
-    	System.out.println(filePath.toString());
     	File file = new File(filePath.toString());
         Scanner inputFile = new Scanner(file);
         // Read lines from the file until no more are left.
@@ -51,11 +56,9 @@ public class DBsrc {
             line = inputFile.nextLine();
             // hostname:port:dbname:username:password
             // 0        1    2      3        4
-            System.out.println(line);
             parts = line.split(":");
             if (parts.length >= 5 && parts[2].equals(dbname) && parts[3].equals(username)) {
             	pss = parts[4].trim();  // deleting end of line
-                System.out.println(pss);	
             }   
         }
         // Close the file.
